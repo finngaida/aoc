00e2fed01ab265c0ff7ebb6b15f0dec3bcf282ab
Finn Gaida <f@fga.pw>
2016-12-07 14:43:28 +0100

    add descriptions

---
+++ Day 4/Security Through Obscurity.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import Foundation
+
+enum E: Error {
+    case unwrap(reason: String)
+}
+
+func getInput() throws -> String {
+    guard let path = Bundle.main.path(forResource: "input", ofType: "txt") else { throw E.unwrap(reason: "Couldn't create path for input file") }
+    guard let input = try? String(contentsOfFile: path) else { throw E.unwrap(reason: "couldn't read input") }
+    return input
+}
